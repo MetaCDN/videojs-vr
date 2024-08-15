422e66488f28f47534afc415a1f38076f96e28cf
Kevin Staunton-Lambert <kevin.staunton-lambert@metacdn.com>
2024-08-15 14:07:01 +1000

    (revert aliasing tests)

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,13 +1,10 @@
-const generate = require('./videojs-vr-generate-rollup-config');
+const generate = require('videojs-generate-rollup-config');
 
 // see https://github.com/videojs/videojs-generate-rollup-config
 // for options
 const config = generate({});
 
-// console.log("KJSL: config=" + config);
-// for (var i in config) {
-//   console.log("KJSL: i=" + i + " config[i]=" + config[i]);
-// }
+// Add additional builds/customization here!
 
 // export the builds to rollup
 export default Object.values(config.builds);
